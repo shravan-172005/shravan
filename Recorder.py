c8f82949a2a5073b4b04315f4d9ae3c2ca7c6766
shravan-172005 <shravanadash.m2023@vitstudent.ac.in>
2025-07-27 15:53:01 +0530

    Recorder.py

--- Recorder.py
+++ Recorder.py
@@ -1,1 +1,11 @@
+import sounddevice 
+from scipy.io.wavfile import write 
 
+fps = 44100 
+duration = <duration in seconds> 
+
+print("recording start") 
+recording = sounddevice.rec(int(duration * fps), samplerate=fps, channels=2) 
+sounddevice.wait() 
+print("recording over") 
+write("<output name>.wav", fps, recording)
